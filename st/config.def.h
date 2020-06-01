86b7893a0e0baa52c5484d9e2919bed118949471
lrr68 <luccaaugusto@protonmail.ch>
2020-06-01 19:08:14 -0300

    mudando transparencia

--- st/config.def.h
+++ st/config.def.h
@@ -84,7 +84,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-unsigned int alpha = 0xaf;
+unsigned int alpha = 0xdf;
 
 #include "/home/lucca/.cache/wal/colors-wal-st.h"
 /* Terminal colors (16 first used in escape sequence)
