c4910bd8f17d9da367694582cd8bce49cc3dff63
lrr68 <luccaaugusto@protonmail.ch>
2020-12-29 20:20:35 -0300

    updating

--- st/config.def.h
+++ st/config.def.h
@@ -84,7 +84,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-unsigned int alpha = 0xdf;
+unsigned int alpha = 0xd8;
 
 #include "/home/lucca/.cache/wal/colors-wal-st.h"
 /* Terminal colors (16 first used in escape sequence)
